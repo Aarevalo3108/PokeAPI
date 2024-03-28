3438b35c037b4016ad4fd505dbd36e215e8a9264
Angel Arevalo <angelarevalo480@gmail.com>
2024-03-28 16:11:54 +0100

    Visualizacion basica lista
    Diseño básico, botón de paginación, imagen e ID listos para visualizar

--- pokeAPI/src/components/App.jsx
+++ pokeAPI/src/components/App.jsx
@@ -1,33 +1,31 @@
 import {useState, useEffect} from 'react'
 import Pokemon from './Pokemon'
+import Pokedex from '../img/pokedex.svg'
 const App = () => {
-  const urlAPI = "https://pokeapi.co/api/v2/pokemon"
-  const [data, setData] = useState([]);
-  const [pages, setPages] = useState(0);
+  const urlAPI = "https://pokeapi.co/api/v2/pokemon?limit=20&offset="
+  const [pokemons, setPokemons] = useState([])
+  const [pages, setPages] = useState(0)
   useEffect(() => {
-    fetch(`${urlAPI}?limit=10&offset=${pages}`)
-      .then((res) => res.json())
-      .then((res) => setData(res))
+    const getPokemones = async (url = urlAPI) => {
+      const response = await fetch(`${url}${pages}`)
+      const listaPoke = await response.json()
+      setPokemons(listaPoke.results)
+    }
+    getPokemones(urlAPI)
   },[pages])
-  if(!data || !data.results) return (
-    <div className="flex flex-col justify-center items-center">
-      <h1 className="text-3xl text-gray-200">Loading...</h1>
-    </div>
-  );
   return (
-    <div className="flex flex-col justify-center items-center gap-8">
-      <div className="flex gap-4 justify-center items-center">
+    <div className="flex flex-col justify-center items-center gap-8 p-8">
+      <h1 className="text-3xl text-gray-200 flex gap-2 items-center justify-center">Pokedex<img src={Pokedex} alt="pokedex" /></h1>
+      <nav className="flex gap-4 justify-center items-center">
         {pages > 1 && (
-          <button className="text-2xl text-gray-200 p-2 bg-blue-500 rounded-full" onClick={() => setPages(pages - 10)}>Previous</button>
+          <button className="text-2xl text-gray-200 p-2 bg-violet-500 rounded-lg" onClick={() => setPages(pages - 20)}>Previous</button>
         )}
         {pages < 1302 && (
-          <button className="text-2xl text-gray-200 p-2 bg-blue-500 rounded-full" onClick={() => setPages(pages + 10)}>Next</button>
+          <button className="text-2xl text-gray-200 p-2 bg-violet-500 rounded-lg" onClick={() => setPages(pages + 20)}>Next</button>
         )}
-      </div>
-      <h1 className="text-3xl text-gray-200">Characters</h1>
-      <div className="grid grid-cols-3 justify-center items-center gap-6">
-      {data.results && <Pokemon url={data.results[0].url} key={data.results[0].name} /> }
-      <img src="poke.sprites.other.dream_world.front_default" alt="" />
+      </nav>
+      <div className="grid grid-cols-5 justify-center items-center gap-6 text-gray-200">
+        {pokemons.map((pokemon) => <Pokemon key={pokemon.name} url={pokemon.url}/>) }
       </div>
     </div>
   )
--- pokeAPI/src/components/Pokemon.jsx
+++ pokeAPI/src/components/Pokemon.jsx
@@ -1,24 +1,30 @@
+/* eslint-disable no-undef */
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react'
 
-const Pokemon = async (url) => {
+const Pokemon = (data = {url}) => {
   const [poke, setPoke] = useState({})
   useEffect(() => {
-    getPokemon(url).then((res) => {
-      setPoke(res)
-    })
-  },[url])
+    const getPokemon = async () => {
+      const response = await fetch(data.url)
+      const resolve = await response.json()
+      const info = {
+        id: resolve.id,
+        name: resolve.name,
+        img: resolve.sprites.other.dream_world.front_default || resolve.sprites.other['official-artwork'].front_default || resolve.sprites.front_default
+      }
+      setPoke(info)
+    }
+    getPokemon()
+  },[])
   return (
-  <div className="flex flex-col justify-center items-center gap-2" key={poke.id}>
-    <h2 className="text-2xl text-gray-200">{poke.name}</h2>
-    <img className="rounded-2xl" alt={poke.name} />
-  </div>
+    <div className="flex flex-col justify-center items-center gap-4 border-r-2 border-b-2 border-gray-900 rounded-lg bg-gray-700 drop-shadow">
+      <h1 className="capitalize bg-green-800 text-gray-200 rounded-lg mt-2 px-2">{poke.name} #{poke.id}</h1>
+      <img className="h-32 w-32 m-4" src={poke.img} alt={poke.name} />
+    </div>
   )
 }
 
-const getPokemon = async (url) => {
-  const poke = await fetch(url);
-  const dataPoke = await poke.json();
-  return dataPoke
-}
+
 
 export default Pokemon
