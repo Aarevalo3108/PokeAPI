63af104459dbf1dfb99ef419790ef352dfb58e4f
Angel Arevalo <angelarevalo480@gmail.com>
2024-04-02 18:03:32 -0400

    Cambios visuales en el componente pokemon
    - Mejoras en la visualizacion de cada pokemon
    - Ahora se muestra las estadisticas y los tipos

--- pokeAPI/src/components/App.jsx
+++ pokeAPI/src/components/App.jsx
@@ -40,13 +40,13 @@ const App = () => {
 <div className="flex flex-col justify-center items-center gap-8 p-8">
   <nav className="flex justify-center items-center sticky z-10 top-0 p-4 text-2xl text-gray-200 flex justify-between gap-2 items-center font-mono w-full h-20 bg-gray-800 rounded">
       <div className='flex flex-row items-center justify-center g-4 max-md:hidden '>
-          <img src={Pokedex} alt="pokedex"  className='max-w-20'/> 
-          <p>Pokedex</p>
+        <img src={Pokedex} alt="pokedex"  className='max-w-20'/> 
+        <p>Pokedex</p>
       </div>
     <form onSubmit={handleSubmit} className="flex flex-row gap-1 bg-white/80 rounded-md py-1 px-1">
-        <img src={Search} alt="" />
-        <input placeholder='Pokebusqueda' type="text" className="bg-inherit focus:outline-none pl-1 text-2xl w-full text-black max-sm:w-60" value={pokemonName} onChange={handleChange}/>
-        <button type='submit'>search</button>
+      <img src={Search} alt="" />
+      <input placeholder='Pokebusqueda' type="text" className="bg-inherit focus:outline-none pl-1 text-2xl w-full text-black max-sm:w-60" value={pokemonName} onChange={handleChange}/>
+      <button type='submit'>search</button>
     </form>
     <img src={Heart} alt="" />
   </nav>
@@ -59,7 +59,7 @@ const App = () => {
           <button className="text-2xl text-gray-200 p-2 bg-green-800 rounded-lg" onClick={() => setPages(pages + 20)}>Next</button>
         )}
        </div>
-        <div className="grid justify-center items-center gap-6 text-gray-200 min-[450px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        <div className="grid justify-center items-center gap-6 text-gray-200 min-[630px]:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {pokemons.map((pokemon) => <Pokemon key={pokemon.name} url={pokemon.url}/>) }
         </div>
         {pokemonData && (
